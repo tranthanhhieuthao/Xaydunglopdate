409641bfda6a0e47cc56626456aeaee4c27e2432
tranthanhhieuthao <tranthanhhieuthao@gmail.com>
2019-05-10 16:53:36 +0700

    bt

---
+++ Xay dung lop Date.js
@@ -0,0 +1,50 @@
+let MyDate= function (day,month,year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+
+    this.getDay = function () {
+        return this.day;
+    }
+    this.getMonth = function () {
+        return this.month;
+    }
+    this.getYear = function () {
+        return this.year;
+    }
+    this.setDay = function (day) {
+        this.day = day;
+    }
+    this.setMonth = function (month) {
+        this.month = month;
+    }
+    this.setYear = function (year) {
+        this.year = year;
+    }
+    this.setDate = function (day,month,year) {
+        this.day=day;
+        this.month=month;
+        this.year=year;
+    }
+    this.ToString =function (day,month,year) {
+        return day+'/'+month+'/'+year;
+    }
+};
+let date = new MyDate();
+// let day = date.getDay();
+// let month = date.getMonth();
+// let year = date.getYear();
+// alert(day + "/" + month + "/" +year);
+date.setDay(15);
+date.setMonth(6);
+date.setYear(2020);
+date.setDate(15,8,2030);
+let day = date.getDay();
+ let month = date.getMonth();
+ let year = date.getYear();
+alert(day + "/" + month + "/" +year);
+let result= date.ToString(12,12,2016);
+alert(result);
+
+
+
